b3f880190e122f0b70e147a32184198db3c58cf8
mreshboboyev <mreshboboyev@gmail.com>
2023-12-17 15:44:13 +0500

    Dependency injection in HomeController.cs for Services

--- DI_Service_Lifetime/Controllers/HomeController.cs
+++ DI_Service_Lifetime/Controllers/HomeController.cs
@@ -10,13 +10,24 @@ namespace DI_Service_Lifetime.Controllers
         private readonly IScopedGuidService _scoped1;
         private readonly IScopedGuidService _scoped2;
         private readonly ISingletonGuidService _singleton1;
-        private readonly IScopedGuidService _singleton2;
+        private readonly ISingletonGuidService _singleton2;
         private readonly ITransientGuidService _transient1;
-        private readonly IScopedGuidService _transient2;
+        private readonly ITransientGuidService _transient2;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IScopedGuidService scoped1,
+            IScopedGuidService scoped2,
+            ISingletonGuidService singleton1,
+            ISingletonGuidService singleton2,
+            ITransientGuidService transient1,
+            ITransientGuidService transient2
+            )
         {
-            _logger = logger;
+            _scoped1 = scoped1;
+            _scoped2 = scoped2;
+            _singleton1 = singleton1;
+            _singleton2 = singleton2;
+            _transient1 = transient1;
+            _transient2 = transient2;
         }
 
         public IActionResult Index()
